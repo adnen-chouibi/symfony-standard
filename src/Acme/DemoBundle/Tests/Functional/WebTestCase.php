0ca17414f798404866737c2764d9449fb4cd35c2
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-07-25 11:34:21 +0200

    updated dependencies

--- src/Acme/DemoBundle/Tests/Functional/WebTestCase.php
+++ src/Acme/DemoBundle/Tests/Functional/WebTestCase.php
@@ -18,54 +18,9 @@ abstract class WebTestCase extends BaseWebTestCase
 
     public function setUp()
     {
-        if (self::$fixturesLoaded) {
-            return;
-        }
-
         $this->loadFixtures(array(
             'Acme\DemoBundle\DataFixtures\ORM\LoadUserData',
-        ), null, 'doctrine');
-
-        self::$fixturesLoaded = true;
-    }
-
-    /**
-     * Callback function to be executed after Schema Execution.
-     * Use this to execute acl:init or other things necessary.
-     */
-    protected function postFixtureSetup()
-    {
-        if (self::$fixturesLoaded) {
-            return;
-        }
-
-        $session = $this->getContainer()->get('doctrine_phpcr')->getConnection();
-        if ($session instanceof Session && $session->getTransport() instanceof DoctrineDBALClient) {
-            $connection = $this->getContainer()->get('doctrine')->getConnection();
-            $schema = RepositorySchema::create();
-            foreach ($schema->toSql($connection->getDatabasePlatform()) as $sql) {
-                $connection->exec($sql);
-            }
-
-            $ntm = $session->getWorkspace()->getNodeTypeManager();
-            $cnd = <<<CND
-// register phpcr_locale namespace
-<phpcr_locale='http://www.doctrine-project.org/projects/phpcr_odm/phpcr_locale'>
-// register phpcr namespace
-<phpcr='http://www.doctrine-project.org/projects/phpcr_odm'>
-[phpcr:managed]
-mixin
-- phpcr:class (STRING)
-- phpcr:classparents (STRING) multiple
-CND
-            ;
-            $ntm->registerNodeTypesCnd($cnd, true);
-
-            foreach ($this->getContainer()->getParameter('doctrine_phpcr.initialize.initializers') as $id) {
-                $initializer = $this->getContainer()->get($id);
-                $initializer->init($session);
-            }
-        }
+        ));
     }
 
     protected function login(Client $client)
